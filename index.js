f146b2b1991d0dca2556874307788cdaaed1f1b3
ss-karthik <maniac.sskarthik@gmail.com>
2024-08-16 19:39:34 +0530

    Initial commit - Basic play/pause and search functions

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import yts from "yt-search";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+});
+
+app.post("/search", async (req,res)=>{
+    const search = req.body.search;
+    const r = await yts( search );
+    const videos = r.videos.slice( 0, 10 );
+    let vidlist = [];
+    let i=0;
+    videos.forEach( function ( v ) {
+        vidlist.push({"ind": i, "title": v.title, "vidid": v.videoId});
+        i++;
+    });
+    
+    res.render("index.ejs", { vidlist: vidlist });
+});
+
+app.post("/stream", async(req,res)=>{
+    console.log(req.body);
+    res.render("index.ejs", { videolink: req.body.video });
+});
+
+
+
+app.listen(port, ()=>{
+    console.log(`Listening at port ${port}`);
+});
